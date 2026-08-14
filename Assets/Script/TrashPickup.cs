2833c907e7f883ea54be50474d7b656710fb1794
np03cy4s260032-niharika <np03cy4s260032@heraldcollege.edu.np>
2026-08-14 12:37:57 +0545

    pickup trash

--- Assets/Script/TrashPickup.cs
+++ Assets/Script/TrashPickup.cs
@@ -4,15 +4,25 @@ public class TrashPickup : MonoBehaviour
 {
     public Camera playerCamera;
     public Transform holdPoint;
-    public float pickupDistance = 3f;
 
-    private GameObject heldItem;
+    public float pickupDistance = 4f;
+    public float throwForce = 8f;
+
+    private GameObject heldTrash;
+    private Rigidbody heldRb;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            TryPickup();
+            if (heldTrash == null)
+            {
+                TryPickup();
+            }
+            else
+            {
+                ThrowTrash();
+            }
         }
     }
 
@@ -27,23 +37,62 @@ public class TrashPickup : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, pickupDistance))
         {
-            Debug.Log("Hit: " + hit.collider.gameObject.name);
+            Transform trashTransform = hit.collider.transform;
 
-            if (hit.collider.CompareTag("Trash"))
+            // Check the object and its parents for the Trash tag
+            if (trashTransform.CompareTag("Trash") ||
+                trashTransform.root.CompareTag("Trash"))
             {
-                heldItem = hit.collider.gameObject;
+                heldTrash = trashTransform.gameObject;
 
-                Rigidbody rb = heldItem.GetComponent<Rigidbody>();
+                // If the collider is on a child object,
+                // use the Rigidbody from the parent
+                heldRb = heldTrash.GetComponent<Rigidbody>();
 
-                if (rb != null)
-                    rb.isKinematic = true;
+                if (heldRb == null)
+                {
+                    heldRb = heldTrash.GetComponentInParent<Rigidbody>();
+                }
 
-                heldItem.transform.SetParent(holdPoint);
-                heldItem.transform.localPosition = Vector3.zero;
-                heldItem.transform.localRotation = Quaternion.identity;
+                if (heldRb == null)
+                {
+                    Debug.Log("Bottle needs a Rigidbody.");
+                    heldTrash = null;
+                    return;
+                }
 
-                Debug.Log("Picked up trash!");
+                heldRb.isKinematic = true;
+                heldRb.useGravity = false;
+
+                heldTrash.transform.SetParent(holdPoint);
+                heldTrash.transform.localPosition = Vector3.zero;
+                heldTrash.transform.localRotation = Quaternion.identity;
+
+                Debug.Log("Trash picked up!");
+            }
+            else
+            {
+                Debug.Log("Object is not tagged Trash. Hit object was: " 
+                    + hit.collider.gameObject.name);
             }
         }
+    }
+
+    void ThrowTrash()
+    {
+        heldTrash.transform.SetParent(null);
+
+        heldRb.isKinematic = false;
+        heldRb.useGravity = true;
+
+        heldRb.AddForce(
+            playerCamera.transform.forward * throwForce,
+            ForceMode.Impulse
+        );
+
+        Debug.Log("Trash thrown!");
+
+        heldTrash = null;
+        heldRb = null;
     }
 }
